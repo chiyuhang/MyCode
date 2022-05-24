58e9b65a1925c54a5b378ef67628bc1492ca07b0
chiyuhang <chiyuhang@bytedance.com>
2022-05-24 17:25:59 +0800

    学习ViewController与MainBoard中元素交互，完成了一个卡牌游戏

---
+++ 0534WarCardGame/0534WarCardGameUITests/_534WarCardGameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _534WarCardGameUITestsLaunchTests.swift
+//  0534WarCardGameUITests
+//
+//  Created by ByteDance on 2022/5/24.
+//
+
+import XCTest
+
+class _534WarCardGameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

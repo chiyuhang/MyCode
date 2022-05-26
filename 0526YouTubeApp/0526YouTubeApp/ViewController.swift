2a07d12ee11efa87965bec8fbc367700b6720956
chiyuhang <chiyuhang@bytedance.com>
2022-05-26 18:06:06 +0800

    初始化YouTobe App project

---
+++ 0526YouTubeApp/0526YouTubeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  0526YouTubeApp
+//
+//  Created by ByteDance on 2022/5/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
